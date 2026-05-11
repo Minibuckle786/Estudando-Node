15abf401bb4d58406ee2f16dd9c579809cee65c8
Emerson de Oliveira <emerson_yeda@hotmail.com>
2026-05-11 20:46:39 -0300

    Iniciando o estudo em node

---
+++ app.js
@@ -0,0 +1,10 @@
+var verdadeiro = true;
+const site = "emerson_yeda@hotmail.com";
+
+console.log("Hello world");
+console.log("Meu nome é Emerson")
+console.log("Eu estou aprendendo Node.js com o Guia do programador");
+
+if (verdadeiro) {
+    console.log(site);
+}
